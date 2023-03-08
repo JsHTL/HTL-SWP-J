e7264dd2f1b62c7d83e737e803579271fcfc7b22
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-08 08:22:36 +0100

    added Console.log

---
+++ 01_Variablen_Operatoren/Variablen_Operatoren.js
@@ -0,0 +1,15 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+let a = 7;
+let b = 8;
+let c = 10;
+let d = 0;
+let e = 0;
+let f = 0;
+
+console.log(a+b+c);
+console.log((a+b)/c);
+console.log(c-a+b);
+console.log(a+b+c*3);
+console.log((a+b+c)/4);
