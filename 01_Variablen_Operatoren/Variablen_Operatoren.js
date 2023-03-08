07ff85c2c0149379c4a0ace29c74872ebb4374fa
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-08 08:26:41 +0100

    added Variable output

--- 01_Variablen_Operatoren/Variablen_Operatoren.js
+++ 01_Variablen_Operatoren/Variablen_Operatoren.js
@@ -4,12 +4,17 @@ console.log("Tolle Sache");
 let a = 7;
 let b = 8;
 let c = 10;
-let d = 0;
-let e = 0;
-let f = 0;
 
 console.log(a+b+c);
 console.log((a+b)/c);
 console.log(c-a+b);
 console.log(a+b+c*3);
 console.log((a+b+c)/4);
+
+a=3;
+b=a+3;
+c=c-3;
+
+console.log(a);
+console.log(b);
+console.log(c);
