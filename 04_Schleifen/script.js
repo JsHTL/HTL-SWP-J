a32e5d4c560cd03ff0c520bce9b4c099d4885189
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-08 09:34:13 +0100

    added whole exercise

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,43 @@
+let a=7;
+let b=9;
+let c=10;
+let d=1234;
+let e=45;
+let f=99;
+let g=0;
+let h=1207;
+let i=1;
+let j=10;
+
+
+let firstname="Jonas"
+
+for(a; a <= e; a++) {
+    console.log(a);
+};
+
+for(b; b <= f; b++) {
+    console.log(b);
+};
+
+for(c; c >= g; c--) {
+    console.log(c);
+};
+
+for(d; d >= h; d--) {
+    console.log(d);
+};
+
+for(i; i<= 8; i++) {
+    console.log(firstname);
+};
+
+for(g; g<= 10; g++) {
+    console.log(g);
+}
+
+console.log(firstname);
+
+for(j; j>= 0; j--) {
+    console.log(j);
+}
