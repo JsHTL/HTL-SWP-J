4033693a9b566848122b8f776e3fa622d4b1737b
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-08 08:39:07 +0100

    added sentence with my name

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let b = 3.9;
 
 console.log("The result of 17.4 divided by 3.9 is "+ a/b);
 
+let firstname = "Jonas";
+let lastname = "Sinn";
+
+console.log("Ich heiße "+ firstname + " " + lastname);
