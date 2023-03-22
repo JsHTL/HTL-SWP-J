e1aa773e95ae0fb139bca45f2b711bafce9e36bf
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-22 08:42:03 +0100

    added random

---
+++ 07_LogischeOperatoren/script.js
@@ -0,0 +1,8 @@
+let randomNumber1 = Math.random() * 100 + 1;
+let random1 = Math.floor(randomNumber1);
+
+let randomNumber2 = Math.random() * 100 + 1;
+let random2 = Math.floor(randomNumber2);
+
+console.log(random1);
+console.log(random2);
