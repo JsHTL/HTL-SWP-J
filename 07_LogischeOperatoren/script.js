e21e8b8c0a423d6a6224f330ed610a2331367394
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-22 08:56:45 +0100

    consolelog added

--- 07_LogischeOperatoren/script.js
+++ 07_LogischeOperatoren/script.js
@@ -5,13 +5,16 @@ let randomNumber2 = Math.random() * 100 + 1;
 let random2 = Math.floor(randomNumber2);
 
 if (random1<random2 && random1<50){
-    alert("Zahl 1 ist kleiner als Zahl 2 und Mini");
+    alert("Zahl 1 ist kleiner als Zahl 2 und Mini.");
+    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini.");
 }
 
 if (random1<30 || random2<30) {
-    alert("Eine der beiden ist kleiner als 30");
+    alert("Eine der beiden ist kleiner als 30.");
+    console.log("Eine der beiden ist kleiner als 30.");
 }
 
 if (random1<50 && random2!=50) {
-    alert("Erste Zahl klein, zweite kein 50iger");
+    alert("Erste Zahl klein, zweite kein 50iger.");
+    console.log("Erste Zahl klein, zweite kein 50iger.")
 }
