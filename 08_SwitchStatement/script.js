5a03779eaa09f1fc2794f0d3217149298cbdf2a4
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-22 09:23:00 +0100

    added ifs

---
+++ 08_SwitchStatement/script.js
@@ -0,0 +1,44 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 5;
+let random1 = Math.floor(randomNumber1);
+
+console.log(random1);
+
+if (random1==1) {
+    console.log("one");
+}
+
+if (random1==2) {
+    console.log("two");
+}
+
+if (random1==3) {
+    console.log("three");
+}
+
+if (random1==4) {
+    console.log("four");
+}
+
+if (random1==5) {
+    console.log("five");
+}
+
+if (random1==6) {
+    console.log("six");
+}
+
+if (random1==7) {
+    console.log("seven");
+}
+
+if (random1==8) {
+    console.log("eight");
+}
+
+if (random1==9) {
+    console.log("nine");
+}
+
+if (random1==10) {
+    console.log("ten");
+}
