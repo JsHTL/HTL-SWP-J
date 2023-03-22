b25272f51f75919575d0f6d1918c6efac58f2b8a
Jonas <jonas.sinn@student.htldornbirn.at>
2023-03-22 08:47:47 +0100

    added ifs

--- 07_LogischeOperatoren/script.js
+++ 07_LogischeOperatoren/script.js
@@ -4,5 +4,14 @@ let random1 = Math.floor(randomNumber1);
 let randomNumber2 = Math.random() * 100 + 1;
 let random2 = Math.floor(randomNumber2);
 
-console.log(random1);
-console.log(random2);
+if (random1<random2 && random1<50){
+    alert("Zahl 1 ist kleiner als Zahl 2 und Mini");
+}
+
+if (random1<30 || random2<30) {
+    alert("Eine der beiden ist kleiner als 30");
+}
+
+if (random1<50 && random2!=50) {
+    alert("Erste Zahl klein, zweite kein 50iger");
+}
