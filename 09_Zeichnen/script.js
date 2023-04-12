7aea72103ceee3cff49a75cea41ac70a8adc3c97
Jonas <jonas.sinn@student.htldornbirn.at>
2023-04-12 08:33:20 +0200

    added first excercise

---
+++ 09_Zeichnen/script.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 2;
+
+for (let index = 1; index < 10; index ++) {
+    console.log("XXXX");
+    console.log("OOOO");
+    console.log("XXXX");
+    console.log("OOOO");
+    if (index%1==0) {
+        console.log("")
+    }
+}
