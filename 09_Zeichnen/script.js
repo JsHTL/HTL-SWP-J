1f8df74f82bb14256cae3bc79acd779cdbb68ec3
Jonas <jonas.sinn@student.htldornbirn.at>
2023-04-12 08:59:53 +0200

    added even and odd

--- 09_Zeichnen/script.js
+++ 09_Zeichnen/script.js
@@ -1,7 +1,7 @@
-let a = 2;
-let b = 2;
+let a = 0;
+let b = 0;
 
-for (let index = 1; index < 10; index ++) {
+for (let index = 1; index < 3; index ++) {
     console.log("XXXX");
     console.log("OOOO");
     console.log("XXXX");
@@ -10,3 +10,15 @@ for (let index = 1; index < 10; index ++) {
         console.log("")
     }
 }
+
+for (let index = 1; index < 101; index ++) {
+    if(index%1==0){
+        a = a + index;
+    }
+    if(index%2==0){
+        b = b + index;
+    }
+}
+
+console.log(a);
+console.log(b);
