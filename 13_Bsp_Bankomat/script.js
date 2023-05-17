585ec9243eb0d66642ee4fe5cfc9bdc339856e5d
Jonas <jonas.sinn@student.htldornbirn.at>
2023-05-17 08:24:42 +0200

    added funktions, also a bug with readLineAsync

--- 13_Bsp_Bankomat/script.js
+++ 13_Bsp_Bankomat/script.js
@@ -14,8 +14,8 @@ const readLineAsync = () => {
     });
 };
 
-let balance = 0;
-let selction = "";
+let selection = true;
+let balance = 1;
 
 
 console.log("Mit den folgenden Zahlen wählen Sie Ihre Aktionen aus.");
@@ -24,29 +24,35 @@ console.log("   2    Abheben");
 console.log("   3    Kontostand");
 console.log("   4    Logout");
 
-let selection = await readLineAsync();
 
-if (selection == 1){
-    console.log("Bitte geben Sie den einzuzahlenden Betrag ein.");
+while(selection){
+    let selction = await readLineAsync();
+    if (selction == 1){
+        console.log("Bitte geben Sie den einzuzahlenden Betrag ein.");
+        let payment = await readLineAsync();
+        balance = balance + payment;
+        console.log("Ihr neuer Kontostand beträgt " + balance + " Euro.")
+    }
 
-    console.log("Sie haben " +  + " Euro eingezahlt, Ihr neuer Kontostand beträgt " + balance + " Euro.")
-}
+    if (selction == 2){
+        console.log("Bitte geben Sie den abzuhebenden Betrag ein.");
+        let payout = await readLineAsync();
+        balance = balance - payout;
+        console.log("Ihr neuer Kontostand beträgt " + balance + " Euro.")
+    }
 
-if (selection == 2){
-    console.log("Bitte geben Sie den abzuhebenden Betrag ein.");
-    
-    console.log("Sie haben " +  + " Euro abgehoben, Ihr neuer Kontostand beträgt " + balance + " Euro.")
-}
+    if (selction == 3){
+        console.log("Ihr aktueller Kontostand beträgt " + balance + " Euro.");
+    }
 
-if (selection == 3){
-    console.log("Ihr aktueller Kontostand beträgt " + balance + " Euro.");
-}
+    if (selction == 4){
+        console.log("Cashmaschine logged out.");
+        selection=false;
+    }
 
-if (selection == 4){
-    console.log("Cashmaschine logged out.");
-    
-}
-
-if (selection =! 1 && selection =! 2 && selection =! 3 && selection =! 4){
+/*if (selction != 1 && selection != 2 && selection != 3 && selection != 4){
     console.log("Bitte geben Sie eine gültige Aktion ein(1, 2, 3, 4)!");
+}*/
 }
+
+readline.close();
