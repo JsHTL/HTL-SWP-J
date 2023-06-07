56b64d64b5b1b5d10ab8939c19ececa6865625cb
Jonas <jonas.sinn@student.htldornbirn.at>
2023-06-07 08:49:02 +0200

    added winCombinations

--- 14_bsp_tiktaktoe/script.js
+++ 14_bsp_tiktaktoe/script.js
@@ -55,6 +55,9 @@ function printField(){
     }
 }
 
+var winCombinations = [field[0][0]&&field[0][1]&&field[0][2], field[1][0]&&field[1][1]&&field[1][2], field[2][0]&&field[2][1]&&field[2][2], field[0][0]&&field[1][1]&&field[2][2], field[2][2]&&field[1][1]&&field[2][0], field[0][0]&&field[1][0]&&field[2][0], field[0][1]&&field[1][1]&&field[2][1], field[0][2]&&field[1][2]&&field[2][2]];
+
+
 function control(){
     
 }
